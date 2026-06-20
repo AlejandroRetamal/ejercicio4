e8e226909ac8fd321a5615ca5879d632ea9edc6f
AlejandroRetamal <al.retamalm@duoc.cl>
2026-06-20 01:05:16 -0400

    taller 4 resuelto

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,26 +1,33 @@
+
 def mostrarMenu():
-    print("=======MENÚ=======")
-    print("1. agregar vehiculo")
-    print("2. buscar vehiculo")
-    print("3. eliminar vehiculo")
-    print("4. actualizar disponibilidad")
-    print("5. mostrar vehiculos")
-    print("6. salir")
-    print("===================")
+    print("========== MENÚ PRINCIPAL ==========")
+    print("1. Agregar vehículo")
+    print("2. Buscar vehículo")
+    print("3. Eliminar vehículo")
+    print("4. Actualizar disponibilidad")
+    print("5. Mostrar vehículos")
+    print("6. Salir")
+    print("=====================================")
 
 def solicitarOpcion():
     while True:
         try:
-            opcion = input("ingrese una opcion: ")
+            opcion = int(input("ingrese una opcion: "))
 
             if opcion < 0 or opcion > 6:
                 print("ingrese una opcion valida (1-6)")
-
-            return opcion
+            else:
+                return opcion
+            
         except ValueError:
             print("ingrese una opcion valida (1-6)")
 
-
+def validarModelo(modeloValidar):
+    if modeloValidar.strip() == "":
+        return False
+    else:
+        return True
+    
 def validarAnio(anioValidar):
     try:
         anioValidar = int(anioValidar)
@@ -29,15 +36,8 @@ def validarAnio(anioValidar):
             return False
         else:
             return True
-        
     except ValueError:
         return False
-
-def validarModelo(modeloValidar):
-    if modeloValidar.strip() == "":
-        return False
-    else:
-        return True
     
 def validarPrecio(precioValidar):
     try:
@@ -47,126 +47,120 @@ def validarPrecio(precioValidar):
             return False
         else:
             return True
-        
     except ValueError:
         return False
 
 def agregarVehiculo(lista):
-    
-    modelo = input("ingrese el modelo del vehiculo: ")
+
+    modelo = input("ingrese modelo del vehiculo: ")
     anio = input("ingrese el año del vehiculo: ")
     precio = input("ingrese el precio del vehiculo: ")
 
-    modeloValido = validarModelo(modelo)
-    anioValido = validarAnio(anio)
-    precioValido = validarPrecio(precio)
+    modeloValidado = validarModelo(modelo)
+    anioValidado = validarAnio(anio)
+    precioValidado = validarPrecio(precio)
 
-    if modeloValido == True and anioValido == True and precioValido == True:    
+    if modeloValidado == True and anioValidado == True and precioValidado == True:
         vehiculo = {
             "modelo": modelo,
             "anio": int(anio),
             "precio": float(precio),
-            "disponible": False           
+            "disponible": False
         }
 
         lista.append(vehiculo)
-    else:
-        print("los datos no cumplen con las validaciones")
 
-def buscarVahiculo(lista, modelo):
+    else:
+        print("los datos no cumplen con la validacion")
+
+
     
+def buscarVehiculo(lista, modelo):
+
     for indice, vehiculo in enumerate(lista):
-        if vehiculo["modelo"] == modelo:
+        if vehiculo['modelo'] == modelo:
             return indice
-    
+        
     return -1
 
 def actualizarVehiculos(lista):
 
     for vehiculo in lista:
         if vehiculo['anio'] >= 2020:
-            vehiculo["disponible"] = True
+            vehiculo['disponible'] = True
         else:
-            vehiculo["disponible"] = False
+            vehiculo['disponible'] = False
 
     print("se actualizaron los registros de vehiculos")
 
-def mostrarVehiculos(lista):
+def MostrarVehiculos(lista):
+
     if len(lista) == 0:
         print("no existen vehiculos registrados aun")
         return
 
     actualizarVehiculos(lista)
 
-    print("===Lista de vehiculos===")
+    print("=== LISTA DE VEHICULOS ===")
     for vehiculo in lista:
-        print(f"modelo: {vehiculo['modelo']}")
-        print(f"precio: {vehiculo['precio']}")
+        print(f"Modelo: {vehiculo['modelo']}")
         print(f"año: {vehiculo['anio']}")
+        print(f"precio: {vehiculo['precio']}")
 
         if vehiculo['disponible']:
-            print(f"disponible?: DISPONIBLE")
+            print("Disponibilidad: DISPONIBLE")
         else:
-            print(f"disponible?: NO DISPONIBLE")
+            print("Disponibilidad: NO DISPONIBLE")
 
 
-
-########## Codigo principal ##########
+####### codigo principal#######
 
 ListaVehiculos = []
 
 while True:
-    
-    mostrarMenu()
 
+    mostrarMenu()
     opcionSeleccionada = solicitarOpcion()
 
     if opcionSeleccionada == 1:
         agregarVehiculo(ListaVehiculos)
 
     elif opcionSeleccionada == 2:
+        
+        vehiculoBuscado = input("ingrese vehiculo a buscar: ")
 
-        modelobuscado = input("ingrese el modelo del vehiculo buscado: ")
-
-        posicion = buscarVahiculo(ListaVehiculos, modelobuscado)
-
+        posicion = buscarVehiculo(ListaVehiculos, vehiculoBuscado)
+    
         if posicion != -1:
-            print(f"vehiculo encontrado en la posicion {posicion}")
-
+            print(f"Vehiculo encontrado en la posicion {posicion}")
             vehiculoEncontrado = ListaVehiculos[posicion]
 
             print(f"modelo: {vehiculoEncontrado['modelo']}")
-            print(f"precio: {vehiculoEncontrado['precio']}")
             print(f"año: {vehiculoEncontrado['anio']}")
-            print(f"disponible?: {vehiculoEncontrado['disponible']}")
+            print(f"precio: {vehiculoEncontrado['precio']}")
+            print(f"disponibilidad: {vehiculoEncontrado['disponible']}")
         else:
-            print(f"el vehiculo '{modelobuscado}' no se encuentra registrado.")
+            print(f"El vehiculo {vehiculoBuscado} no se encuentra registrado")
 
     elif opcionSeleccionada == 3:
-        
-        modelobuscado = input("ingrese el modelo del vehiculo buscado: ")
+        vehiculoBuscado = input("ingrese vehiculo a buscar: ")
 
-        posicion = buscarVahiculo(ListaVehiculos, modelobuscado)
+        posicion = buscarVehiculo(ListaVehiculos, vehiculoBuscado)              
 
         if posicion != -1:
             ListaVehiculos.pop(posicion)
 
-            print(f"vehiculo con modelo '{modelobuscado}' eliminado")
+            print(f"vehiculo {vehiculoBuscado} eliminado")
         else:
-            print(f"el vehiculo '{modelobuscado}' no se encuentra registrado.")
+            print(f"El vehículo {vehiculoBuscado} no se encuentra registrado.")
+
 
     elif opcionSeleccionada == 4:
         actualizarVehiculos(ListaVehiculos)
 
     elif opcionSeleccionada == 5:
-        mostrarVehiculos(ListaVehiculos)
+        MostrarVehiculos(ListaVehiculos)
 
     elif opcionSeleccionada == 6:
-        print("gracias por usar nuestro sistema, vuelva pronto.")
+        print("Gracias por usar el sistema. Vuelva Pronto")
         break
-
-
-
-
-
-        
